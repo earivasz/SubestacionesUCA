feb6620b29afe8eeaf95b259b28507ab3b700e0f
Jose Ramirez <r2jf@hotmail.com>
2013-06-24 23:54:55 -0600

    empece a modificar el ver subestaciones
    Para mostrar la info, estilos van hasta despues

--- application/views/subestaciones/detalle.php
+++ application/views/subestaciones/detalle.php
@@ -1,11 +1,45 @@
-<div id="contenido">
+<script>
+    var neighborhoods = [
+    <?php 
+    $contSubEst = count($subest);
+    $cont = 1;
+    foreach ($subest as $subest_item): 
+        if($cont<$contSubEst){
+    ?>
+        new google.maps.LatLng(<?php echo $subest_item['coordX'] ?>, <?php echo $subest_item['coordY'] ?>),    
+    <?php 
+        }else{
+    ?>
+        new google.maps.LatLng(<?php echo $subest_item['coordX'] ?>, <?php echo $subest_item['coordY'] ?>)
+    <?php
+        }
+    endforeach;
+        
+        foreach ($subest as $subest_item) :
+                //$cX = $subest_item['coordX'];
+                //$cY = $subest_item['coordY'];
+                $numSub = $subest_item['numSubestacion'];
+                $localizacion = $subest_item['localizacion'];
+                $capacidad = $subest_item['capacidad'];
+                $conexion = $subest_item['conexion'];
+                $activo = $subest_item['activo'];
+            endforeach;  
+
+            
+    ?>
+    ];
+    setTimeout(drop_modificar(), 1000);
     
-    <div id="overlay-mapa">
+</script>
+<h2>Detalle Subestacion</h2>
+<div id="map-canvas" class="map-create"></div>
+
+    
+    
             <?php foreach ($subest as $subest_item): ?>
             <?php echo $subest_item['idSubestacion'] ?>
         <br>
             <?php echo $subest_item['localizacion'] ?>
         <br><br>
             <?php endforeach ?>
-    </div>
-</div>
+
