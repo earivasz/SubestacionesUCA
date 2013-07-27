3437f69ac2e7e5b0bfe1bb279233d79d03724a85
Eduardo Rivas <zapa_1013@hotmail.com>
2013-07-27 17:17:34 -0600

    cargado de usuarios desde la base

--- application/views/users/administracion.php
+++ application/views/users/administracion.php
@@ -1,125 +1,140 @@
-<script type="text/javascript" src="<?=base_url()?>js/jquery-1.10.1.min.js"></script>
-<script type="text/javascript" src="<?=base_url()?>js/jqxcore.js"></script>
-<script type="text/javascript" src="<?=base_url()?>js/jqxdata.js"></script>
-<script type="text/javascript" src="<?=base_url()?>js/jqxbuttons.js"></script>
-<script type="text/javascript" src="<?=base_url()?>js/jqxscrollbar.js"></script>
-<script type="text/javascript" src="<?=base_url()?>js/jqxlistbox.js"></script>
-<script type="text/javascript" src="<?=base_url()?>js/jqxdropdownlist.js"></script>
-<script type="text/javascript" src="<?=base_url()?>js/jqxmenu.js"></script>
-<script type="text/javascript" src="<?=base_url()?>js/jqxgrid.js"></script>
-<script type="text/javascript" src="<?=base_url()?>js/jqxgrid.filter.js"></script>
-<script type="text/javascript" src="<?=base_url()?>js/jqxgrid.sort.js"></script>
-<script type="text/javascript" src="<?=base_url()?>js/jqxgrid.selection.js"></script>
-<script type="text/javascript" src="<?=base_url()?>js/jqxpanel.js"></script>
-<script type="text/javascript" src="<?=base_url()?>js/jqxcalendar.js"></script>
-<script type="text/javascript" src="<?=base_url()?>js/jqxdatetimeinput.js"></script>
-<script type="text/javascript" src="<?=base_url()?>js/jqxcheckbox.js"></script>
-<script type="text/javascript" src="<?=base_url()?>js/gettheme.js"></script>
-<script type="text/javascript">
-function generatedata(rowscount, hasNullValues) {
-    // prepare the data
-    var data = new Array();
-    if (rowscount === undefined) rowscount = 100;
-    var firstNames =
-    [
-        "Andrew", "Nancy", "Shelley", "Regina", "Yoshi", "Antoni", "Mayumi", "Ian", "Peter", "Lars", "Petra", "Martin", "Sven", "Elio", "Beate", "Cheryl", "Michael", "Guylene"
-    ];
+<script type="text/javascript" src="<?=base_url()?>jqwidgets/jqxcore.js"></script>
+    <script type="text/javascript" src="<?=base_url()?>jqwidgets/jqxbuttons.js"></script>
+    <script type="text/javascript" src="<?=base_url()?>jqwidgets/jqxscrollbar.js"></script>
+    <script type="text/javascript" src="<?=base_url()?>jqwidgets/jqxmenu.js"></script>
+    <script type="text/javascript" src="<?=base_url()?>jqwidgets/jqxgrid.js"></script>
+    <script type="text/javascript" src="<?=base_url()?>jqwidgets/jqxgrid.selection.js"></script> 
+    <script type="text/javascript" src="<?=base_url()?>jqwidgets/jqxgrid.columnsresize.js"></script> 
+    <script type="text/javascript" src="<?=base_url()?>jqwidgets/jqxdata.js"></script> 
+    <script type="text/javascript" src="<?=base_url()?>jqwidgets/jqxcheckbox.js"></script> 
+    <script type="text/javascript" src="<?=base_url()?>jqwidgets/jqxlistbox.js"></script> 
+    <script type="text/javascript" src="<?=base_url()?>jqwidgets/gettheme.js"></script>
 
-    var lastNames =
-    [
-        "Fuller", "Davolio", "Burke", "Murphy", "Nagase", "Saavedra", "Ohno", "Devling", "Wilson", "Peterson", "Winkler", "Bein", "Petersen", "Rossi", "Vileid", "Saylor", "Bjorn", "Nodier"
-    ];
-
-    var productNames =
-    [
-        "Black Tea", "Green Tea", "Caffe Espresso", "Doubleshot Espresso", "Caffe Latte", "White Chocolate Mocha", "Caramel Latte", "Caffe Americano", "Cappuccino", "Espresso Truffle", "Espresso con Panna", "Peppermint Mocha Twist"
-    ];
-
-    var priceValues =
-    [
-         "2.25", "1.5", "3.0", "3.3", "4.5", "3.6", "3.8", "2.5", "5.0", "1.75", "3.25", "4.0"
-    ];
-
-    for (var i = 0; i < rowscount; i++) {
-        var row = {};
-        var productindex = Math.floor(Math.random() * productNames.length);
-        var price = parseFloat(priceValues[productindex]);
-        var quantity = 1 + Math.round(Math.random() * 10);
-
-        row["id"] = i;
-        row["available"] = productindex % 2 === 0;
-        if (hasNullValues === true) {
-            if (productindex % 2 !== 0) {
-                var random = Math.floor(Math.random() * rowscount);
-                row["available"] = i % random === 0 ? null : false;
-            }
-        }
-        row["firstname"] = firstNames[Math.floor(Math.random() * firstNames.length)];
-        row["lastname"] = lastNames[Math.floor(Math.random() * lastNames.length)];
-        row["name"] = row["firstname"] + " " + row["lastname"]; 
-        row["productname"] = productNames[productindex];
-        row["price"] = price;
-        row["quantity"] = quantity;
-        row["total"] = price * quantity;
-
-        var date = new Date();
-        date.setFullYear(2013, Math.floor(Math.random() * 11), Math.floor(Math.random() * 27));
-        date.setHours(0, 0, 0, 0);
-        row["date"] = date;
-       
-        data[i] = row;
-    }
-
-    return data;
-}    
+<script>
     
-$(document).ready(function () {
-            var theme = getDemoTheme();
-
-            var data = generatedata(500);
+    var chart;
+    var arreglo_completo = new Array();
+    var arreglo_pag = new Array();
+    var myGrid;
+  window.onload = function () {
+    //******************CARGA DE TABLA
+    
+   arreglo_completo = <?php echo json_encode($usuarios); ?>;
+    //AQUI IBA EL CODIGO DEL GRID ANTERIOR
+    
+    var theme = getDemoTheme();
+            
             var source =
             {
-                localdata: data,
-                datafields:
-                [
-                    { name: 'name', type: 'string' },
-                    { name: 'productname', type: 'string' },
-                    { name: 'available', type: 'bool' },
-                    { name: 'date', type: 'date'},
-                    { name: 'quantity', type: 'number' }
-                ],
+                localdata: arreglo_completo,
                 datatype: "array"
             };
-
-            var dataAdapter = new $.jqx.dataAdapter(source);
-
+            var dataAdapter = new $.jqx.dataAdapter(source, {
+                downloadComplete: function (data, status, xhr) { },
+                loadComplete: function (data) { },
+                loadError: function (xhr, status, error) { }
+            });
             $("#jqxgrid").jqxGrid(
             {
-                width: 685,
+                width: 900,
                 source: dataAdapter,
-                showfilterrow: true,
-                filterable: true,
-                theme: theme,
-                selectionmode: 'multiplecellsextended',
                 columns: [
-                  { text: 'Name', columntype: 'textbox', filtertype: 'textbox', filtercondition: 'starts_with', datafield: 'name', width: 115 },
-                  {
-                      text: 'Product', filtertype: 'checkedlist', datafield: 'productname', width: 220
-                  },
-                  { text: 'Available', datafield: 'available', columntype: 'checkbox', filtertype: 'bool', width: 67 },
-                  { text: 'Ship Date', datafield: 'date', filtertype: 'date', width: 210, cellsalign: 'right', cellsformat: 'd' },
-                  { text: 'Qty.', datafield: 'quantity', filtertype: 'number',  cellsalign: 'right' }
+                  { text: 'First Name', datafield: 'usuario', width: 100 },
+                  { text: 'Last Name', datafield: 'nomUser', width: 100 },
+                  { text: 'Product', datafield: 'apel', width: 180 },
+                  { text: 'Quantity', datafield: 'correo', width: 80, cellsalign: 'right' },
+                  { text: 'Unit Price', datafield: 'estado', width: 90, cellsalign: 'right', cellsformat: 'c2' },
+                  { text: 'Total', datafield: 'ultimoIngreso', width: 100, cellsalign: 'right', cellsformat: 'c2' },
+                  { text: 'First Name', datafield: 'idPerfil', width: 100 }
                 ]
             });
-            $('#clearfilteringbutton').jqxButton({ height: 25, theme: theme });
-            $('#clearfilteringbutton').click(function () {
-                $("#jqxgrid").jqxGrid('clearfilters');
-            });
-        });
-</script>
+            
+            var listSource = [{ label: 'Name', value: 'name', checked: false }, { label: 'Beverage Type', value: 'type', checked: true }, { label: 'Calories', value: 'calories', checked: true }, { label: 'Total Fat', value: 'totalfat', checked: true }, { label: 'Protein', value: 'protein', checked: true}];
 
-<div id="contenido">
-    <div id="jqxgrid">
+            $("#jqxlistbox").jqxListBox({ source: listSource, width: 200, height: 200, theme: theme, checkboxes: true });
+            $("#jqxlistbox").on('checkChange', function (event) {
+                if (event.args.checked) {
+                    $("#jqxgrid").jqxGrid('showcolumn', event.args.value);
+                }
+                else {
+                    $("#jqxgrid").jqxGrid('hidecolumn', event.args.value);
+                }
+            });
+    
+    
+    
+  };
+  
+//  var paginacion =function(indice){
+//      console.log(indice);
+//      myGrid.load(arreglo_pag[indice], "jsarray");
+//      myGrid.parse(arreglo_pag[indice], "jsarray");
+//      console.log(arreglo_pag[indice]);
+////    var myHonda = { color: "red", wheels: 4, engine: { cylinders: 4, size: 2.2 } };
+////    var myCar = [myHonda, 2, "cherry condition", "purchased 1997"];
+////    var newCar = myCar.slice(0, 2);
+////    //console.log(newCar);
+
+//  }
+  
+  var recargar =function(){
+  //alert("hola que aseee");
+            
+                chart.options.data[0].dataPoints = <?php
+                $datos2 = array('label: "cambio1", y: 28', 'label: "cambio2", y: 15',);
+            $top2 = count($datos2);
+            $cont2 = 1;
+            echo '[';
+            foreach ($datos2 as $itemGraf) {
+                if($cont2 < $top2)
+                    echo '{' . $itemGraf . '},';
+                else
+                    echo '{' . $itemGraf . '}';
+                $cont2++;
+            }
+            echo ']';
+            ?>;
+            chart.render();
+    
+  };
+  
+  
+  </script>
+<h2>GRAFICOS</h2>
+<div id='jqxWidget'>
+        
+        <div style="width:100%; height: 600px;" id="jqxgrid"></div>
+        <br>
+        <div style="" id="jqxlistbox"></div>
     </div>
-    <input style="margin-top: 10px;" value="Remove Filter" id="clearfilteringbutton" type="button" />
-</div>
+<br>
+<button onClick="recargar();" name="pressme">Press Me</button>
+
+<?php
+//    foreach ($datos as $value) {
+//     echo $value;
+//    }
+    
+    //echo '<br>' . json_encode($datos) ;
+    echo '<br>';
+    //echo $datos_reales_json;
+    //echo $datos_simulados_json;
+    echo "<br>";
+    //echo json_encode($headers);
+    echo "<br>";
+    //echo json_encode($widths);
+    echo "<br>";
+    //echo $datos_simulados;
+    
+//    $top = count($datos);
+//    $cont = 1;
+//    echo '[';
+//    foreach ($datos as $itemGraf) {
+//        if($cont < $top)
+//            echo '{' . $itemGraf . '},';
+//        else
+//            echo '{' . $itemGraf . '}';
+//        $cont++;
+//    }
+//    echo ']';
+?>
