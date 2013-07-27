c66c1db7d9f99c4ed2a411cdd54a3ae5c57da89b
Jose Ramirez <r2jf@hotmail.com>
2013-07-27 17:02:24 -0600

    cambio de grid
    Cambio de Grid, usando ahora el jqgrid

--- application/views/subestaciones/graficos.php
+++ application/views/subestaciones/graficos.php
@@ -1,8 +1,21 @@
+<script type="text/javascript" src="<?=base_url()?>jqwidgets/jqxcore.js"></script>
+    <script type="text/javascript" src="<?=base_url()?>jqwidgets/jqxbuttons.js"></script>
+    <script type="text/javascript" src="<?=base_url()?>jqwidgets/jqxscrollbar.js"></script>
+    <script type="text/javascript" src="<?=base_url()?>jqwidgets/jqxmenu.js"></script>
+    <script type="text/javascript" src="<?=base_url()?>jqwidgets/jqxgrid.js"></script>
+    <script type="text/javascript" src="<?=base_url()?>jqwidgets/jqxgrid.selection.js"></script> 
+    <script type="text/javascript" src="<?=base_url()?>jqwidgets/jqxgrid.columnsresize.js"></script> 
+    <script type="text/javascript" src="<?=base_url()?>jqwidgets/jqxdata.js"></script> 
+    <script type="text/javascript" src="<?=base_url()?>jqwidgets/jqxcheckbox.js"></script> 
+    <script type="text/javascript" src="<?=base_url()?>jqwidgets/jqxlistbox.js"></script> 
+    <script type="text/javascript" src="<?=base_url()?>jqwidgets/gettheme.js"></script>
+
 <script>
     
     var chart;
     var arreglo_completo = new Array();
     var arreglo_pag = new Array();
+    var myGrid;
     <?php
     
     //los datos entrarian linea por linea (cada linea es un conjunto de datos separados por comas)
@@ -11,15 +24,16 @@
     //el tipo de grafica dependera del valor que venga como parametro en la llamda de la pagina.
     
     //SIMULANDO DATOS:
-    $simulacion = "4/22/2013 11:31,75.55185,0.451633,2.369664,0.261574,8.541767,0.121345,4.581907,0.113815,1.671975,0.09726,2.875276,0.089291,0.538053,0.073813,0.483467,0.0703,0.760839,0.076046,0.487163,0.082853,0.260822,0.076809,0.311854,0.238721,0.420766,0.130676,0.199594,0.270461,0.177867,0.079403,0.312972,0.07109,0.130364,0.069402,0.195517,0.076076,0.242641,0.076742,0.127689,0.085107";
+    $simulacion = "4/22/20131131,75.55185,0.451633,2.369664,0.261574,8.541767,0.121345,4.581907,0.113815,1.671975,0.09726,2.875276,0.089291,0.538053,0.073813,0.483467,0.0703,0.760839,0.076046,0.487163,0.082853,0.260822,0.076809,0.311854,0.238721,0.420766,0.130676,0.199594,0.270461,0.177867,0.079403,0.312972,0.07109,0.130364,0.069402,0.195517,0.076076,0.242641,0.076742,0.127689,0.085107";
     $datos_simulados = array();
-    for($yy=0;$yy<1250;$yy++){
+    for($yy=0;$yy<1203;$yy++){
         array_push($datos_simulados, explode(",", $simulacion));
     }
     //$datos_simulados_json = json_encode($datos_simulados);
-    $arreglo_paginacion = array();
+    //$arreglo_paginacion = array();
     //el siguiente numero va a ser dinamico, por el momento es de prueba y estatico
     $numColumnas = 40;
+    $numFilas = 1203;
     //esta variable tambien sera dinamica, dependiendo del tipo de tabla que este viendo seran los headers
     //$headers = "fecha hora,fundamental,2 armonico,3 armonico,4 armonico,5 armonico,6 armonico,7 armonico,8 armonico,9 armonico,10 armonico,11 armonico,12 armonico,13 armonico,14 armonico,15 armonico,16 armonico,17 armonico,18 armonico,19 armonico,20 armonico,21 armonico";
     $headers = "col1, col2, col3, col4, col5,col1, col2, col3, col4, col5,col1, col2, col3, col4, col5,col1, col2, col3, col4, col5,col1, col2, col3, col4, col5,col1, col2, col3, col4, col5,col1, col2, col3, col4, col5,col1, col2, col3, col4, col40";
@@ -83,59 +97,103 @@
     
     //******************CARGA DE TABLA
     <?php 
-    $arreglo_paginacion = json_encode(array_slice($datos_simulados, 600, 200));
-    ?>
+    //aqui voy a generar un arreglo con partes del arreglo total, para habilitar la paginacion
+    for($pp=0;$pp<($numFilas/200);$pp++){?>
+            arreglo_pag.push(<?php echo json_encode(array_slice($datos_simulados, ($pp * 200), 200)); ?>);
+            <?php
+    }?>
     
-    myGrid = new dhtmlXGridObject("gridContainer");
-    myGrid.setImagePath("<?=base_url()?>img/imgs_xgrid/");
-    myGrid.enableResizing("true");
-    myGrid.setHeader(<?php echo json_encode($headers); ?>);
-    //myGrid.setHeader("col1, col2, col3, col4, col5, col6");
-    //myGrid.setInitWidths("70,70,70,70,70,70");
-    myGrid.setInitWidths(
-        <?php
-        $widths = "120,";
-        for($ww=1;$ww<$numColumnas;$ww++){
-            $widths = $widths . "70,";
-        }
-        $widths = $widths . "70";
-        echo json_encode($widths);
-        ?>
-        );
+   arreglo_completo = <?php echo json_encode($datos_simulados); ?>;
+    //AQUI IBA EL CODIGO DEL GRID ANTERIOR
+    
+    var theme = getDemoTheme();
             
+            var source =
+            {
+                localdata: arreglo_completo,
+                datatype: "array"
+            };
+            var dataAdapter = new $.jqx.dataAdapter(source, {
+                downloadComplete: function (data, status, xhr) { },
+                loadComplete: function (data) { },
+                loadError: function (xhr, status, error) { }
+            });
+            $("#jqxgrid").jqxGrid(
+            {
+                width: 900,
+                source: dataAdapter,
+                columns: [
+                  { text: 'First Name', datafield: '0', width: 100 },
+                  { text: 'Last Name', datafield: '1', width: 100 },
+                  { text: 'Product', datafield: '2', width: 180 },
+                  { text: 'Quantity', datafield: '3', width: 80, cellsalign: 'right' },
+                  { text: 'Unit Price', datafield: '4', width: 90, cellsalign: 'right', cellsformat: 'c2' },
+                  { text: 'Total', datafield: '5', width: 100, cellsalign: 'right', cellsformat: 'c2' },
+                  { text: 'First Name', datafield: '6', width: 100 },
+                  { text: 'Last Name', datafield: '7', width: 100 },
+                  { text: 'Product', datafield: '8', width: 180 },
+                  { text: 'Quantity', datafield: '9', width: 80, cellsalign: 'right' },
+                  { text: 'Unit Price', datafield: '10', width: 90, cellsalign: 'right', cellsformat: 'c2' },
+                  { text: 'First Name', datafield: '11', width: 100 },
+                  { text: 'Last Name', datafield: '12', width: 100 },
+                  { text: 'Product', datafield: '13', width: 180 },
+                  { text: 'Quantity', datafield: '14', width: 80, cellsalign: 'right' },
+                  { text: 'Unit Price', datafield: '15', width: 90, cellsalign: 'right', cellsformat: 'c2' },
+                  { text: 'First Name', datafield: '16', width: 100 },
+                  { text: 'Last Name', datafield: '17', width: 100 },
+                  { text: 'Product', datafield: '18', width: 180 },
+                  { text: 'Quantity', datafield: '19', width: 80, cellsalign: 'right' },
+                  { text: 'Unit Price', datafield: '20', width: 90, cellsalign: 'right', cellsformat: 'c2' },
+                  { text: 'First Name', datafield: '21', width: 100 },
+                  { text: 'Last Name', datafield: '22', width: 100 },
+                  { text: 'Product', datafield: '23', width: 180 },
+                  { text: 'Quantity', datafield: '24', width: 80, cellsalign: 'right' },
+                  { text: 'Unit Price', datafield: '25', width: 90, cellsalign: 'right', cellsformat: 'c2' },
+                  { text: 'First Name', datafield: '26', width: 100 },
+                  { text: 'Last Name', datafield: '27', width: 100 },
+                  { text: 'Product', datafield: '28', width: 180 },
+                  { text: 'Quantity', datafield: '29', width: 80, cellsalign: 'right' },
+                  { text: 'Unit Price', datafield: '30', width: 90, cellsalign: 'right', cellsformat: 'c2' },
+                  { text: 'First Name', datafield: '31', width: 100 },
+                  { text: 'Last Name', datafield: '32', width: 100 },
+                  { text: 'Product', datafield: '33', width: 180 },
+                  { text: 'Quantity', datafield: '34', width: 80, cellsalign: 'right' },
+                  { text: 'Unit Price', datafield: '35', width: 90, cellsalign: 'right', cellsformat: 'c2' },
+                  { text: 'First Name', datafield: '36', width: 100 },
+                  { text: 'Last Name', datafield: '37', width: 100 },
+                  { text: 'Product', datafield: '38', width: 180 },
+                  { text: 'Quantity', datafield: '39', width: 80, cellsalign: 'right' },
+                  { text: 'Unit Price', datafield: '40', width: 90, cellsalign: 'right', cellsformat: 'c2' },
+                ]
+            });
+            
+            var listSource = [{ label: 'Name', value: 'name', checked: false }, { label: 'Beverage Type', value: 'type', checked: true }, { label: 'Calories', value: 'calories', checked: true }, { label: 'Total Fat', value: 'totalfat', checked: true }, { label: 'Protein', value: 'protein', checked: true}];
+
+            $("#jqxlistbox").jqxListBox({ source: listSource, width: 200, height: 200, theme: theme, checkboxes: true });
+            $("#jqxlistbox").on('checkChange', function (event) {
+                if (event.args.checked) {
+                    $("#jqxgrid").jqxGrid('showcolumn', event.args.value);
+                }
+                else {
+                    $("#jqxgrid").jqxGrid('hidecolumn', event.args.value);
+                }
+            });
     
-    //myGrid.setColAlign("left,right,right");
-    myGrid.init();
-    myGrid.setSkin("dhx_skyblue");
-    myGrid.load(<?php echo $arreglo_paginacion; ?>, "jsarray");
-    myGrid.parse(<?php echo $arreglo_paginacion; ?>, "jsarray");
-    //******************FIN CARGA DE TABLA
-//    alert("hola que aseee");
     
     
-    arreglo_completo = <?php echo json_encode($datos_simulados); ?>;
-    //console.log(arreglo_completo);
   };
   
-  var paginacion =function(indice){
-      indice = indice * 200;
-      var arr;
-      arreglo_pag = arreglo_completo.slice(indice, 200);
-      arr = JSON.parse(arreglo_completo);
-      //arr = arr.slice(indice, 200);
-      //console.log(indice);
-      //console.log(arreglo_completo);
-      console.log(arr);
-      
-    var myHonda = { color: "red", wheels: 4, engine: { cylinders: 4, size: 2.2 } };
-    var myCar = [myHonda, 2, "cherry condition", "purchased 1997"];
-    var newCar = myCar.slice(0, 2);
-    //console.log(newCar);
+//  var paginacion =function(indice){
+//      console.log(indice);
+//      myGrid.load(arreglo_pag[indice], "jsarray");
+//      myGrid.parse(arreglo_pag[indice], "jsarray");
+//      console.log(arreglo_pag[indice]);
+////    var myHonda = { color: "red", wheels: 4, engine: { cylinders: 4, size: 2.2 } };
+////    var myCar = [myHonda, 2, "cherry condition", "purchased 1997"];
+////    var newCar = myCar.slice(0, 2);
+////    //console.log(newCar);
 
-      
-      //AQUI ME QUEDE VIENDO POR QUE DEMONIOS NO ME HACE EL SLICE!!
-      
-  }
+//  }
   
   var recargar =function(){
   //alert("hola que aseee");
@@ -163,15 +221,13 @@
 <h2>GRAFICOS</h2>
 <div id="chartContainer" style="height: 300px; width: 100%;">
   </div>
-<div id="gridContainer" style="height: 600px; width: 100%; overflow:hidden;"></div>
-<div id="pagingContainer" style="height: 40px; width: 100%; margin-top: 15px; margin-left: 30px;">
-    <?php
-    for($bb=0;$bb<(1250/200);$bb++){
-        echo '<a href="#gridContainer" onClick="paginacion(' . $bb . ');"><div class="cuadro-paginacion">' . ($bb + 1) . '</div></a>';
-    }
-    ?>
-    <div id="posicion-paginacion" style="float:left;"> ---- Mostrando registros del x al x de xxxx</div>
-</div>
+<div id='jqxWidget'>
+        
+        <div style="width:100%; height: 600px;" id="jqxgrid"></div>
+        <br>
+        <div style="" id="jqxlistbox"></div>
+    </div>
+<br>
 <button onClick="recargar();" name="pressme">Press Me</button>
 
 <?php
@@ -184,11 +240,11 @@
     //echo $datos_reales_json;
     //echo $datos_simulados_json;
     echo "<br>";
-    echo json_encode($headers);
+    //echo json_encode($headers);
     echo "<br>";
-    echo json_encode($widths);
+    //echo json_encode($widths);
     echo "<br>";
-    echo json_encode($datos_simulados);
+    //echo $datos_simulados;
     
 //    $top = count($datos);
 //    $cont = 1;
